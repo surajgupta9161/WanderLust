7bacd8af38aac22fdd84fd98223053bf29d7c53b
surajgupta <suraj666189@gmail.com>
2025-12-21 14:19:01 +0530

    Add user router.route

--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,9 @@ const { saveRedirectUrl } = require("../middleware");
 const userRoute = express.Router()
 const userController = require("../controllers/users")
 
-userRoute.get("/signup", userController.signupForm)
+userRoute.route("/signup").get(userController.signupForm).post(userController.signup)
 
-userRoute.post("/signup", userController.signup)
-
-userRoute.get("/login", userController.loginForm)
-
-userRoute.post("/login", saveRedirectUrl, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), userController.login)
+userRoute.route("/login").get(userController.loginForm).post(saveRedirectUrl, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), userController.login)
 
 userRoute.get("/logout", userController.logout)
 
